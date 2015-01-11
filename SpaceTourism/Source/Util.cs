5f69feb88ad0f03e4b68d44072233344fe4b34f2
MoreUmpf <league_3@web.de>
2015-01-11 22:37:05 +0100

    Added Globals & Moved TourismEvents

--- SpaceTourism/Source/Util.cs
+++ SpaceTourism/Source/Util.cs
@@ -12,180 +12,52 @@ using UnityEngine;
 
 namespace SpaceTourism
 {
-	public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+	public static class Globals
 	{
-		static T _instance;
-		static object _lock = new object();
-		static bool applicationIsQuitting = false;
-	 
-		public static T Instance
+		public static List<Type> ContractTypes
 		{
 			get
 			{
-				if (applicationIsQuitting) 
-				{
-					Debug.LogWarning("[Singleton] Instance '"+ typeof(T) +
-						"' already destroyed on application quit." +
-						" Won't create again - returning null.");
-					return null;
-				}
-	 
-				lock(_lock)
-				{
-					if (_instance == null)
-					{
-						_instance = (T) FindObjectOfType(typeof(T));
-	 
-						if ( FindObjectsOfType(typeof(T)).Length > 1 )
-						{
-							Debug.LogError("[Singleton] Something went really wrong " +
-								" - there should never be more than 1 singleton!" +
-								" Reopenning the scene might fix it.");
-							return _instance;
-						}
-	 
-						if (_instance == null)
-						{
-							GameObject singleton = new GameObject();
-							_instance = singleton.AddComponent<T>();
-							singleton.name = "(singleton) "+ typeof(T).ToString();
-	 
-							DontDestroyOnLoad(singleton);
-	 
-							Debug.Log("[Singleton] An instance of " + typeof(T) + 
-								" is needed in the scene, so '" + singleton +
-								"' was created with DontDestroyOnLoad.");
-						} 
-						else 
-						{
-							Debug.Log("[Singleton] Using instance already created: " +
-								_instance.gameObject.name);
-						}
-					}
-					return _instance;
-				}
+				if (contractTypes == null)
+					GetTypes();
+				
+				return contractTypes;
 			}
 		}
 		
-		public void OnDestroy () 
+		public static List<Type> PhaseTypes
 		{
-			applicationIsQuitting = true;
+			get
+			{
+				if (phaseTypes == null)
+					GetTypes();
+				
+				return phaseTypes;
+			}
+		}
+		
+		static List<Type> contractTypes;
+		static List<Type> phaseTypes;
+		
+		private static void GetTypes()
+		{
+			contractTypes = new List<Type>();
+			phaseTypes = new List<Type>();
+			
+			foreach (var loadedAssembly in AssemblyLoader.loadedAssemblies)
+			{
+				var types = loadedAssembly.assembly.GetTypes();
+				phaseTypes.AddRange(types.Where(type => type.IsSubclassOf(typeof(TourismPhase))));
+				contractTypes.AddRange(types.Where(type => type.GetInterface("ITourismContract") != null));
+			}
 		}
 	}
 	
-	public class ReadOnlyDictionary<TKey, TValue> : IDictionary<TKey, TValue>
+	public static class TourismEvents
 	{
-	    readonly IDictionary<TKey, TValue> _dictionary;
-	
-	    public ReadOnlyDictionary()
-	    {
-	        _dictionary = new Dictionary<TKey, TValue>();
-	    }
-	
-	    public ReadOnlyDictionary(IDictionary<TKey, TValue> dictionary)
-	    {
-	        _dictionary = dictionary;
-	    }
-	
-	    void IDictionary<TKey, TValue>.Add(TKey key, TValue value)
-	    {
-	        throw ReadOnlyException();
-	    }
-	
-	    public bool ContainsKey(TKey key)
-	    {
-	        return _dictionary.ContainsKey(key);
-	    }
-	
-	    public ICollection<TKey> Keys
-	    {
-	        get { return _dictionary.Keys; }
-	    }
-	
-	    bool IDictionary<TKey, TValue>.Remove(TKey key)
-	    {
-	        throw ReadOnlyException();
-	    }
-	
-	    public bool TryGetValue(TKey key, out TValue value)
-	    {
-	        return _dictionary.TryGetValue(key, out value);
-	    }
-	
-	    public ICollection<TValue> Values
-	    {
-	        get { return _dictionary.Values; }
-	    }
-	
-	    public TValue this[TKey key]
-	    {
-	        get
-	        {
-	            return _dictionary[key];
-	        }
-	    }
-	
-	    TValue IDictionary<TKey, TValue>.this[TKey key]
-	    {
-	        get
-	        {
-	            return this[key];
-	        }
-	        set
-	        {
-	            throw ReadOnlyException();
-	        }
-	    }
-	
-	    void ICollection<KeyValuePair<TKey, TValue>>.Add(KeyValuePair<TKey, TValue> item)
-	    {
-	        throw ReadOnlyException();
-	    }
-	
-	    void ICollection<KeyValuePair<TKey, TValue>>.Clear()
-	    {
-	        throw ReadOnlyException();
-	    }
-	
-	    public bool Contains(KeyValuePair<TKey, TValue> item)
-	    {
-	        return _dictionary.Contains(item);
-	    }
-	
-	    public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
-	    {
-	        _dictionary.CopyTo(array, arrayIndex);
-	    }
-	
-	    public int Count
-	    {
-	        get { return _dictionary.Count; }
-	    }
-	
-	    public bool IsReadOnly
-	    {
-	        get { return true; }
-	    }
-	
-	    bool ICollection<KeyValuePair<TKey, TValue>>.Remove(KeyValuePair<TKey, TValue> item)
-	    {
-	        throw ReadOnlyException();
-	    }
-	
-	    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
-	    {
-	        return _dictionary.GetEnumerator();
-	    }
-
-	    IEnumerator IEnumerable.GetEnumerator()
-	    {
-	        return GetEnumerator();
-	    }
-	
-	    private static Exception ReadOnlyException()
-	    {
-	        return new NotSupportedException("This dictionary is read-only");
-	    }
+		public static EventData<KerbalTourist, KerbalTourist.KerbalState, KerbalTourist.KerbalState> onTouristStateChange = new EventData<KerbalTourist, KerbalTourist.KerbalState, KerbalTourist.KerbalState>("TouristStateChange");
+		public static EventData<ProtoVessel> onStationCompleted = new EventData<ProtoVessel>("StationCompleted");
+		public static EventData<ProtoVessel> onBaseCompleted = new EventData<ProtoVessel>("BaseCompleted");
 	}
 }
 
